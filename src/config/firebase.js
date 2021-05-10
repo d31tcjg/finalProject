254bbeb41c903bb7052a3882818a2e1a45561446
d31tcjg <41980420+d31tcjg@users.noreply.github.com>
2021-05-10 17:42:53 -0500

    Delete firebase.js

--- src/config/firebase.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAJ7PUOvbgZBKyNq729rYNeIsaYOUX4dss",
-  authDomain: "finalproject-1ceda.firebaseapp.com",
-  projectId: "finalproject-1ceda",
-  storageBucket: "finalproject-1ceda.appspot.com",
-  messagingSenderId: "701383052329",
-  appId: "1:701383052329:web:6b79802363b383e75f6038",
-  measurementId: "G-4KDY6TYSKS",
-};
